0078304c2490a1f5e35a0f006f5d88702bdf83f0
khomotso gojela <motsoak49@gmail.com>
2023-08-10 19:48:46 +0200

    fixed errors IWA11

--- scripts.js
+++ scripts.js
@@ -1,34 +1,42 @@
 // script.js
 
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
+const root1 = document.querySelector('[data-key="order1"]')
+const biscuits1 = document.querySelector('[data-key="order1"] .biscuits .count')
+const donuts1 = document.querySelector('[data-key="order1"] .donuts .count')
+const pancakes1 = document.querySelector('[data-key="order1"] .pancakes .count')
+const status1 = document.querySelector('[data-key="order1"] .status dd:nth-child(2)')
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
+const root2 = document.querySelector('[data-key="order2"]')
+const biscuits2 = document.querySelector('[data-key="order2"] .biscuits .count')
+const donuts2 = document.querySelector('[data-key="order2"] .donuts .count')
+const pancakes2 = document.querySelector('[data-key="order2"] .pancakes .count')
+const status2 = document.querySelector('[data-key="order2"] .status dd:nth-child(2)')
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+const root3 = document.querySelector('[data-key="order3"]')
+const biscuits3 = document.querySelector('[data-key="order3"] .biscuits .count')
+const donuts3 = document.querySelector('[data-key="order3"] .donuts .count')
+const pancakes3 = document.querySelector('[data-key="order3"] .pancakes .count')
+const status3 = document.querySelector('[data-key="order3"] .status dd:nth-child(2)')
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+biscuits1.textContent = '10'
+donuts1.textContent = '13'
+pancakes1.textContent = '0'
+status1.textContent = 'Delivered : false'
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+// 2-biscuits= 2-root.biscuits,
+// 2-donuts = 2-root.donuts,
+// 2-pancakes = 2-root.pancakes,
+// 2-status = 2-root.status ? Delivered : Pending
+biscuits2.textContent = '5'
+donuts2.textContent = '0'
+pancakes2.textContent = '2'
+status2.textContent = 'Delivered : false'
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+// 3-biscuits= 3-root.biscuits,
+// 3-donuts = 3-root.donuts,
+// 3-pancakes = 3-root.pancakes,
+// 3-status = 3-root.status ? Delivered : Pending
+biscuits3.textContent = '12'
+donuts3.textContent = '11'
+pancakes3.textContent = '15'
+status3.textContent = 'Delivered : true'
